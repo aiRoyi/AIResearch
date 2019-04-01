49bc78125b3a5cdef81da7968f12695abe24e5ba
cmy <a4527943@qq.com>
2019-04-01 23:17:54 +0800

    print all cities aqi

--- airqualityindex.py
+++ airqualityindex.py
@@ -1,67 +1,43 @@
 import requests
-
-
-def cal_linear(iaqi_lo, iaqi_hi, bp_lo, bp_hi, cp):
-    """
-    范围缩放
-    """
-    iaqi = (iaqi_hi - iaqi_lo) * (cp - bp_lo) / (bp_hi - bp_lo) + iaqi_lo
-    return iaqi
-
-
-def cal_pm_iaqi(pm_val):
-    if 0 <= pm_val <36:
-        iaqi = cal_linear(0, 50, 0, 35, pm_val)
-    if 36 <= pm_val < 76:
-        iaqi = cal_linear(50, 100, 35, 75, pm_val)
-    if 76 <= pm_val < 116:
-        iaqi = cal_linear(100, 150, 75, 115, pm_val)
-    else:
-        pass
-    return iaqi
-
-
-def cal_co_iaqi(co_val):
-    if 0 <= co_val < 3:
-        iaqi = cal_linear(0, 50, 0, 2, co_val)
-    if 3 <= co_val < 5:
-        iaqi = cal_linear(50, 100, 2, 4, co_val)
-    else:
-        pass
-    return iaqi
-
-
-def cal_aqi(param_list):
-    pm_val = param_list[0]
-    co_val = param_list[1]
-    pm_iaqi = cal_pm_iaqi(pm_val)
-    co_iaqi = cal_co_iaqi(co_val)
-    iaqi_list = []
-    iaqi_list.append(pm_iaqi)
-    iaqi_list.append(co_iaqi)
-    return max(iaqi_list)
+from bs4 import BeautifulSoup
 
 
 def get_html_text(url):
     r = requests.get(url, timeout=30)
-    print(r.status_code)
     return r.text
 
 
-def main():
-    city_pinyin = input("请输入城市拼音：")
+def get_all_cities():
+    url = "http://pm25.in/"
+    soup = BeautifulSoup(get_html_text(url), 'lxml')
+    city_div = soup.find_all('div', {'class': 'bottom'})[1]
+    city_link_list = city_div.find_all('a')
+    city_list = []
+    for city_link in city_link_list:
+        city_name = city_link.text
+        city_pinyin = city_link['href'][1:]
+        city_list.append((city_name, city_pinyin))
+    return city_list
+
+
+def get_city_aqi(city_pinyin):
     url = "http://pm25.in/" + city_pinyin
-    url_text = get_html_text(url)
-    print(url_text)
-    aqi_div = '''<div class="span12 data">
-        <div class="span1">
-          <div class="value">
-            '''
-    index = url_text.find(aqi_div)
-    begin_index = index + len(aqi_div)
-    end_index = url_text.find("</div>", begin_index)
-    aqi_val = url_text[begin_index: end_index]
-    print("空气质量为：{}".format(aqi_val))
+    soup = BeautifulSoup(get_html_text(url), 'lxml')
+    div_list = soup.find_all('div', {'class': 'span1'})
+    city_aqi = []
+    for i in range(0, 8):
+        div_content = div_list[i]
+        caption = div_content.find('div', {'class': 'caption'}).text.strip()
+        value = div_content.find('div', {'class': 'value'}).text.strip()
+        city_aqi.append((caption, value))
+    return city_aqi
+
+
+def main():
+    city_list = get_all_cities()
+    for city in city_list:
+        city_aqi = get_city_aqi(city[1])
+        print("{}的空气质量为：{}".format(city[0], city_aqi))
 
 
 if __name__ == '__main__':
