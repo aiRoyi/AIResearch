ede68524b70f8d5e4a9df11c7f18b4d29a62b44f
cmy <a4527943@qq.com>
2019-04-10 22:38:18 +0800

    add data visualization

--- aqi_analysis.py
+++ aqi_analysis.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
 
 
 def main():
@@ -9,25 +13,23 @@ def main():
     print('数据预览：')
     print(aqi_data.head())
 
+    # 数据清洗
+    # 只保留AQI>0的数据
+    # filter_condition = aqi_data['AQI'] > 0
+    # clean_aqi_data = aqi_data[filter_condition]
+
+    clean_aqi_data = aqi_data[aqi_data['AQI'] > 0]
+
     # 基本统计
-    print('AQI最大值：', aqi_data['AQI'].max())
-    print('AQI最小值：', aqi_data['AQI'].min())
-    print('AQI均值：', aqi_data['AQI'].mean())
+    print('AQI最大值：', clean_aqi_data['AQI'].max())
+    print('AQI最小值：', clean_aqi_data['AQI'].min())
+    print('AQI均值：', clean_aqi_data['AQI'].mean())
 
-    # top10
-    top10_cities = aqi_data.sort_values(by=['AQI']).head(10)
-    print('空气质量最好的10个城市：')
-    print(top10_cities)
-
-    # bottom10
-    # bottom10_cities = aqi_data.sort_values(by=['AQI']).tail(10)
-    bottom10_cities = aqi_data.sort_values(by=['AQI'], ascending=False).head(10)
-    print('空气质量最差的10个城市：')
-    print(bottom10_cities)
-
-    # 保存成csv
-    top10_cities.to_csv('top10_aqi.csv', index=False)
-    bottom10_cities.to_csv('bottom10_aqi.csv', index=False)
+    # top50可视化
+    top50_cities = clean_aqi_data.sort_values(by=['AQI']).head(50)
+    top50_cities.plot(kind='bar', x='City', y='AQI', title='空气质量最好的50个城市', figsize=(20, 10))
+    plt.savefig('top50_aqi_bar.png')
+    plt.show()
 
 
 if __name__ == '__main__':
